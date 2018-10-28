7b79536c4d53dd8713b968eee8fd2ab8d262fc17
Arsalan Sadri <asadri62@gmail.com>
2018-10-28 16:56:30 -0700

    updating the routes with express.Router

--- routes/api/user/user.js
+++ routes/api/user/user.js
@@ -1,10 +1,8 @@
 // const jwt = require("jsonwebtoken");
-// const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt");
 // const userAuth = require("../../../middleware/user-auth");
 // const db = require("../../../models/user");
 // if (process.env.NODE_ENV !== 'production') require("dotenv").load();
-
-
 
 const router = require("express").Router();
 const userControlller = require("../../../controllers/userController");
@@ -13,10 +11,18 @@ router
     .route("/")
     .get(function (req, res, next) {
         res.send("It just works!");
-});
+    });
 
 router
     .route("/sign-up")
-    .post(userControlller.create);
+    .post((req, res) => {
+        bcrypt.hash(req.body.password, 10, function (err, encrypted) {
+            if (err) res.sendStatus(400);
+            else {
+                req.body.password = encrypted;
+                userControlller.create(req, res);
+            }
+        });
+    });
 
 module.exports = router;
