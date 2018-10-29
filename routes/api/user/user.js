2379fe0e5f58f34de4f44c3680b9a85aff528be6
Arsalan Sadri <asadri62@gmail.com>
2018-10-28 22:09:18 -0700

    minor changes

--- routes/api/user/user.js
+++ routes/api/user/user.js
@@ -1,9 +1,3 @@
-// const jwt = require("jsonwebtoken");
-// const bcrypt = require("bcrypt");
-// const userAuth = require("../../../middleware/user-auth");
-// const db = require("../../../models/user");
-// if (process.env.NODE_ENV !== 'production') require("dotenv").load();
-
 const router = require("express").Router();
 const userControlller = require("../../../controllers/userController");
 
